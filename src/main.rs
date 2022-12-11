0f776a29d1c8dc97183d09dfa46882763be85e0b
Sherlock Holmes <sherlockholmesxp@gmail.com>
2022-12-11 21:31:17 +0800

    made random

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 use std::env;
+use rand;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let text = args.get(1).expect("ERR 01 -- Unable to get argument");
     let mut cringetext = String::new();
-    let mut capital = false;
+    let mut capital: bool = rand::random::<bool>();
     for char in text.chars() {
         if capital == false {
             cringetext.push(char.to_ascii_lowercase());
-            capital = true;
+            capital = rand::random();
         } else {
             cringetext.push(char.to_ascii_uppercase());
-            capital = false;
+            capital = rand::random();
         }
     }
     println!("{}", cringetext);
